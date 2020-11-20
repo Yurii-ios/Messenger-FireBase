037fd5eda6ac49032bfa8e661f803cba397d7c63
Yurii <Yurii>
2020-11-20 12:53:35 +0100

    Initial Commit

---
+++ Messenger + FireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger + FireBase
+//
+//  Created by Yurii Sameliuk on 20/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
