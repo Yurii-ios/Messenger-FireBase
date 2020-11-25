c63bd4d63200848cbc72da3ae32f65047ac1c01a
Yurii <Yurii>
2020-11-25 21:36:43 +0100

    create class ActiveChatCell to create a custom cell

---
+++ Messenger + FireBase/Controllers/Cell/ActiveChatCell.swift
@@ -0,0 +1,12 @@
+//
+//  ActiveChatCell.swift
+//  Messenger + FireBase
+//
+//  Created by Yurii Sameliuk on 25/11/2020.
+//
+
+import UIKit
+
+class ActiveChatCell: UICollectionViewCell {
+    
+}
