3ec8f87b417a63a544e41637ef2aed15133d3ebd
Yurii <Yurii>
2020-11-26 19:16:06 +0100

    create class  GradientView

---
+++ Messenger + FireBase/Extension/GradientView.swift
@@ -0,0 +1,55 @@
+//
+//  GradientView.swift
+//  Messenger + FireBase
+//
+//  Created by Yurii Sameliuk on 26/11/2020.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    enum Point {
+        case topLeading
+        case leading
+        case bottomLeading
+        case top
+        case center
+        case bottom
+        case topTrailing
+        case trailing
+        case bottomTrailing
+
+        var point: CGPoint {
+            switch self {
+            case .topLeading:
+                return CGPoint(x: 0, y: 0)
+            case .leading:
+                return CGPoint(x: 0, y: 0.5)
+            case .bottomLeading:
+                return CGPoint(x: 0, y: 1.0)
+            case .top:
+                return CGPoint(x: 0.5, y: 0)
+            case .center:
+                return CGPoint(x: 0.5, y: 0.5)
+            case .bottom:
+                return CGPoint(x: 0.5, y: 1.0)
+            case .topTrailing:
+                return CGPoint(x: 1.0, y: 0.0)
+            case .trailing:
+                return CGPoint(x: 1.0, y: 0.5)
+            case .bottomTrailing:
+                return CGPoint(x: 1.0, y: 1.0)
+            }
+        }
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
